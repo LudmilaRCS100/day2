4a8baa7381b27a57dcdc2fba3ade02f327e5bb37
Ludmila <luvi_lv@inbox.lv>
2018-05-07 20:04:27 +0300

    Day 2
    Pirmais commits 2. dienai/proekta izveide

---
+++ day_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String dati;
+            Console.WriteLine("Ludzu ievadiet dzimsanas datus!");
+            dati = Console.ReadLine();
+            Console.WriteLine("Jus esat dzimis " + dati);
+            Console.ReadLine();
+
+
+        }
+    }
+}
